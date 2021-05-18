fdc79d3a96886fb3a931f99be8ac16d6ab9ed69a
dannyXSC <874794016@qq.com>
2021-05-18 19:48:26 +0800

    init

---
+++ Youpass/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Youpass.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
