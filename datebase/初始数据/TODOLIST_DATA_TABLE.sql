e1496e3d205c9b6cee39cc39b2c36fa001575957
Yxxxb <2947934317@qq.com>
2021-09-10 18:37:28 +0800

    Initial

---
+++ datebase/初始数据/TODOLIST_DATA_TABLE.sql
@@ -0,0 +1,11 @@
+REM INSERTING into C##TEST.TODOLIST
+SET DEFINE OFF;
+Insert into C##TEST.TODOLIST (PERSON_ID,ID,DESCRIPTION) values ('10500',1,'周二上数据结构的课程');
+Insert into C##TEST.TODOLIST (PERSON_ID,ID,DESCRIPTION) values ('10500',2,'周五上数据结构实验设计课程');
+Insert into C##TEST.TODOLIST (PERSON_ID,ID,DESCRIPTION) values ('10500',3,'批改数据结构作业作业');
+Insert into C##TEST.TODOLIST (PERSON_ID,ID,DESCRIPTION) values ('1950002',1,'完成数据库课程设计');
+Insert into C##TEST.TODOLIST (PERSON_ID,ID,DESCRIPTION) values ('1950002',2,'完成操作系统课程设计');
+Insert into C##TEST.TODOLIST (PERSON_ID,ID,DESCRIPTION) values ('1950002',3,'上创项目报告书');
+Insert into C##TEST.TODOLIST (PERSON_ID,ID,DESCRIPTION) values ('1950002',4,'进行数据库原理与应用考试');
+Insert into C##TEST.TODOLIST (PERSON_ID,ID,DESCRIPTION) values ('1950002',5,'进行数据库愿你与应用项目答辩');
+Insert into C##TEST.TODOLIST (PERSON_ID,ID,DESCRIPTION) values ('1950003',1,'数据库课程设计答辩');
