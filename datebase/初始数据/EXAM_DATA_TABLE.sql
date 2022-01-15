b95978cdb5c6b4ab1b5d87b2b21011cc8488de2b
Yxxxb <2947934317@qq.com>
2022-01-16 00:03:46 +0800

    update

--- datebase/初始数据/EXAM_DATA_TABLE.sql
+++
@@ -1,6 +0,0 @@
-REM INSERTING into C##TEST.EXAM
-SET DEFINE OFF;
-Insert into C##TEST.EXAM (COURSE_ID,EXAM_ID,START_TIME,END_TIME,NATURE,TITLE) values ('420243',2,to_date('16-7月 -21','DD-MON-RR'),to_date('16-7月 -21','DD-MON-RR'),0,'数据库测试考试2');
-Insert into C##TEST.EXAM (COURSE_ID,EXAM_ID,START_TIME,END_TIME,NATURE,TITLE) values ('420243',1,to_date('16-7月 -21','DD-MON-RR'),to_date('16-7月 -21','DD-MON-RR'),0,'数据库测试考试');
-Insert into C##TEST.EXAM (COURSE_ID,EXAM_ID,START_TIME,END_TIME,NATURE,TITLE) values ('420245',1,to_date('15-7月 -21','DD-MON-RR'),to_date('16-7月 -21','DD-MON-RR'),0,'操作系统第一次考试');
-Insert into C##TEST.EXAM (COURSE_ID,EXAM_ID,START_TIME,END_TIME,NATURE,TITLE) values ('420240',1,to_date('15-7月 -21','DD-MON-RR'),to_date('15-7月 -21','DD-MON-RR'),0,'期末考试');
